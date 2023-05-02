b13e02de202252325cc126e9e33f6835dcd90024
Adrián <adrianlomu2004@gmail.com>
2023-05-02 16:35:12 +0200

    hotfix(trigger.sql): modificado INSERT & DELETE.

--- trigger.sql
+++ trigger.sql
@@ -1,7 +1,15 @@
--- TRIGGER ejecuta funcion "actualizar_total_libros" cada vez que se inserta/elimina/modifica un libro
-CREATE TRIGGER llamar_procedimiento
+-- TRIGGER ejecuta funcion "llamar_procedimiento_insertarLibro" cada vez que se inserta un libro
+DELIMITER $$ CREATE TRIGGER llamar_procedimiento_insertarLibro
 AFTER
 INSERT ON `libros` FOR EACH ROW BEGIN
 DECLARE total_libros INT;
 SET total_libros = actualizar_total_libros();
-END
+END $$ DELIMITER;
+
+-- TRIGGER ejecuta funcion "llamar_procedimiento_eliminarLibro" cada vez que se elimine un libro
+DELIMITER $$ CREATE TRIGGER llamar_procedimiento_eliminarLibro
+AFTER
+DELETE ON `libros` FOR EACH ROW BEGIN
+DECLARE total_libros INT;
+SET total_libros = actualizar_total_libros();
+END $$ DELIMITER;
